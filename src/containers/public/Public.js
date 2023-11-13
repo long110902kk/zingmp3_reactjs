02ed8136ee4a03fb4364af6f98804d2d720d0fd4
Long <duonghailong785@gmail.com>
2023-11-13 09:10:03 +0700

    v12

--- src/containers/public/Public.js
+++ src/containers/public/Public.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import { SidebarLeft, SidebarRight } from "../../components";
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Player, SidebarLeft, SidebarRight } from '../../components'
 
 const Public = () => {
-    return ( 
-        <div className='w-full flex'>
-            <div className='w-[240px] flex-none border border-blue-500'>
-                <SidebarLeft/>
+    return (
+        <div className='w-full min-h-screen flex flex-col bg-main-300'>
+            <div className='w-full h-full flex flex-auto'>
+                <div className='w-[240px] min-h-sreen flex-none border border-blue-500'>
+                    <SidebarLeft />
+                </div>
+                <div className='flex-auto border border-red-500'>
+                    <Outlet />
+                </div>
+                <div className='w-[329px] flex-none border border-green-500 '>
+                    <SidebarRight />
+                </div>
             </div>
-            <div className='flex-auto border border-red-500'>
-                <Outlet/>
-            </div>
-            <div className='w-[329px] flex-none border boder-green-500'>
-                <SidebarRight/>
+            <div className='flex-none h-[90px]'>
+                <Player />
             </div>
         </div>
-     );
+    )
 }
- 
-export default Public;
+export default Public
--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,6 +1,6 @@
 const actionTypes = {
     GET_HOME: 'GET_HOME',
-    GET_BANNERS: 'GET_BANNERS'
+    SET_CUR_SONG_ID: 'SET_CUR_SONG_ID'
 }
 
 export default actionTypes
